1e449a906db40249a21ba1fe978b0553bec5af4e
lopestorm <guillopes2002@gmail.com>
2023-09-03 19:54:55 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Tab_10_Pro-user
-add_lunch_combo omni_Tab_10_Pro-userdebug
-add_lunch_combo omni_Tab_10_Pro-eng
+#add_lunch_combo omni_Tab_10_Pro-user
+#add_lunch_combo omni_Tab_10_Pro-userdebug
+#add_lunch_combo omni_Tab_10_Pro-eng
