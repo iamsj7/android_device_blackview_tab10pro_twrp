c7ee3813c7fd06b01fe68de110a38c21a6ec1e5b
lopestorm <guillopes2002@gmail.com>
2023-09-03 19:56:17 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Tab_10_Pro.mk
+    $(LOCAL_DIR)/twrp_Tab_10_Pro.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Tab_10_Pro-user \
-    omni_Tab_10_Pro-userdebug \
-    omni_Tab_10_Pro-eng
+    twrp_Tab_10_Pro-user \
+    twrp_Tab_10_Pro-userdebug \
+    twrp_Tab_10_Pro-eng
